6871eefcf0160a385c56ccdc4852759936c1304d
Alex Stephen <stepa@umich.edu>
2014-02-16 01:00:46 -0500

    Sublime for macs only

--- symlink.sh
+++ symlink.sh
@@ -63,10 +63,12 @@ info 'installing dotfiles'
 
   done
 
+if [ "$(uname -s)" == "Darwin" ]
+then
 # Sublime Text
-ln -s ~/.dotfiles/sublime/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
-ln -s ~/.dotfiles/sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 2
-ln -s ~/.dotfiles/sublime/Settings ~/Library/Application\ Support/Sublime\ Text\ 2
-ln -s ~/.dotfiles/sublime/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
-ln -s "~/.dotfiles/sublime/Backup" ~/Library/Application\ Support/Sublime\ Text\ 2
-
+	ln -s ~/.dotfiles/sublime/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
+	ln -s ~/.dotfiles/sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 2
+	ln -s ~/.dotfiles/sublime/Settings ~/Library/Application\ Support/Sublime\ Text\ 2
+	ln -s ~/.dotfiles/sublime/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
+	ln -s "~/.dotfiles/sublime/Backup" ~/Library/Application\ Support/Sublime\ Text\ 2
+fi
