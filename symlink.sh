584887e44b76d564e9c682c098a52f85e786889d
Alex Stephen <stepa@umich.edu>
2014-02-16 01:06:58 -0500

    Using holman's bootstrap script

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,60 @@
-info 'installing dotfiles'
+#!/usr/bin/env bash
+#
+# bootstrap installs things.
+
+DOTFILES_ROOT="`pwd`"
+
+set -e
+
+echo ''
+
+info () {
+  printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+setup_gitconfig () {
+  if ! [ -f git/gitconfig.symlink ]
+  then
+    info 'setup gitconfig'
+
+    git_credential='cache'
+    if [ "$(uname -s)" == "Darwin" ]
+    then
+      git_credential='osxkeychain'
+    fi
+
+    user ' - What is your github author name?'
+    read -e git_authorname
+    user ' - What is your github author email?'
+    read -e git_authoremail
+
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.symlink.example > git/gitconfig.symlink
+
+    success 'gitconfig'
+  fi
+}
+
+link_files () {
+  ln -s $1 $2
+  success "linked $1 to $2"
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
 
   overwrite_all=false
   backup_all=false
@@ -62,13 +118,34 @@ info 'installing dotfiles'
     fi
 
   done
+}
 
+setup_gitconfig
+install_dotfiles
+
+# If we're on a Mac, let's install and setup homebrew.
 if [ "$(uname -s)" == "Darwin" ]
 then
-# Sublime Text
-	ln -s ~/.dotfiles/sublime/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
-	ln -s ~/.dotfiles/sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 2
-	ln -s ~/.dotfiles/sublime/Settings ~/Library/Application\ Support/Sublime\ Text\ 2
-	ln -s ~/.dotfiles/sublime/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
-	ln -s "~/.dotfiles/sublime/Backup" ~/Library/Application\ Support/Sublime\ Text\ 2
+  info "installing dependencies"
+  if . bin/dot > /tmp/dotfiles-dot 2>&1
+  then
+    success "dependencies installed"
+  else
+    fail "error installing dependencies"
+  fi
 fi
+
+echo ''
+echo '  All installed!'
+
+sublime (){
+  if [ "$(uname -s)" == "Darwin" ]
+  then
+   # Sublime Text
+	 ln -s ~/.dotfiles/sublime/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
+	 ln -s ~/.dotfiles/sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 2
+	 ln -s ~/.dotfiles/sublime/Settings ~/Library/Application\ Support/Sublime\ Text\ 2
+	 ln -s ~/.dotfiles/sublime/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2
+	 ln -s "~/.dotfiles/sublime/Backup" ~/Library/Application\ Support/Sublime\ Text\ 2
+  fi
+}
