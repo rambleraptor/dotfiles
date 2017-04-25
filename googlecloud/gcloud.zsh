f98f131cb43489e86c70540ad94a3a8af757ec15
Alex Stephen <alexstephen@google.com>
2017-04-25 14:29:51 -0700

    i do work for google cloud so it should be in dotfiles

---
+++ googlecloud/gcloud.zsh
@@ -0,0 +1,14 @@
+# Gcloud SSH
+function gcloud-tunnel(){
+  gcloud compute ssh $1 --ssh-flag="-L $2:localhost:$2"
+}
+
+function gssh() {
+  gcloud compute ssh $1
+}
+
+function gcopy-home(){
+  gcloud compute copy-files $1 $2:~
+}
+
+alias gcompute="gcloud compute"
