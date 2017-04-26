44497e32eb20dc811669f7e9d2121fbc1831b8fd
Alex Stephen <astephen2@gmail.com>
2017-04-25 23:41:08 -0700

    new aliases and I don't use python

---
+++ alias.zsh
@@ -0,0 +1,18 @@
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias -- -="cd -"
+
+# git alias
+alias g="git"
+
+# copy file interactive
+alias cp='cp -i'
+
+# move file interactive
+alias mv='mv -i'
+
+# untar
+alias untar='tar xvf'
--- python/python.zsh
+++
@@ -1,6 +0,0 @@
-#Virtualenv
-if [ -r /usr/local/bin/virtualenvwrapper.sh ]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-    export VIRTUAL_ENV_DISABLE_PROMPT=1
-fi
-
