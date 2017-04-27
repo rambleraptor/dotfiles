aef7dfdc485cff0a7c410ebad7fda5cf105b4c65
Alex Stephen <alexstephen@google.com>
2017-04-27 11:28:21 -0700

    new git aliases and a git cleaning utility

--- alias.zsh
+++ alias.zsh
@@ -8,6 +8,10 @@ alias -- -="cd -"
 # git aliases
 alias g="git"
 alias master="git checkout master"
+alias gaa="git add --all"
+alias gca="git commit --amend"
+
+alias gerrit="git push origin HEAD:refs/for/master --recurse-submodules=no"
 
 # copy file interactive
 alias cp='cp -i'
