7abc9337af9456b0bf4d454669aac27c670f420b
Alex Stephen <astephen2@gmail.com>
2017-05-17 23:43:20 -0700

    changed gcloud function to alias

--- googlecloud/gcloud.zsh
+++ googlecloud/gcloud.zsh
@@ -3,9 +3,7 @@ function gcloud-tunnel(){
   gcloud compute ssh $1 --ssh-flag="-L $2:localhost:$2"
 }
 
-function gssh() {
-  gcloud compute ssh $1
-}
+alias gssh="gcloud compute ssh $1"
 
 alias gcompute="gcloud compute"
 alias gcp="gcloud compute copy-files $1 $2~"
