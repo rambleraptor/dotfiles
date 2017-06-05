f6439c4b413c1b9ff657f0155f5d8ac459ce806d
Alex Stephen <alexstephen@google.com>
2017-06-05 14:22:06 -0700

    fixing homebrew install script and making it better

--- macos/install.sh
+++ macos/install.sh
@@ -2,10 +2,10 @@
 source $DOTFILES/bootstrap/lib/helpers.sh
 
 if ask "install homebrew"; then
-  $HOME/.dotfiles/bootstrap/lib/macos/homebrew.sh
+  $HOME/.dotfiles/install/homebrew.sh
 fi
 
 if ask "enable macosx defaults"; then
   running "enabling osx defaults"
-  $HOME/.dotfiles/bootstrap/lib/macos/defaults.sh
+  $HOME/.dotfiles/macos/install/defaults.sh
 fi
--- macos/install/homebrew.sh
+++ macos/install/homebrew.sh
@@ -6,34 +6,32 @@ source $HOME/.dotfiles/bootstrap/lib/helpers.sh
 # install homebrew
 #####
 
-running "checking homebrew install"
-brew_bin=$(which brew) 2>&1 > /dev/null
-if [[ $? != 0 ]]; then
-    running "installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    if [[ $? != 0 ]]; then
-        error "unable to install homebrew, script $0 abort!"
-        exit -1
-    fi
-fi
-ok
+#running "checking homebrew install"
+#brew_bin=$(which brew) 2>&1 > /dev/null
+#if [[ $? != 0 ]]; then
+#    running "installing homebrew"
+#    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#    if [[ $? != 0 ]]; then
+#        error "unable to install homebrew, script $0 abort!"
+#        exit -1
+#    fi
+#fi
+#ok
+#
+#running "checking brew-cask install"
+#output=$(brew tap | grep cask)
+#if [[ $? != 0 ]]; then
+#    running "installing brew-cask"
+#    require_brew caskroom/cask/brew-cask
+#fi
+#ok
 
-running "checking brew-cask install"
-output=$(brew tap | grep cask)
-if [[ $? != 0 ]]; then
-    running "installing brew-cask"
-    require_brew caskroom/cask/brew-cask
-fi
-ok
-
-if ask "install fonts"; then
-  running "installing fonts"
-  brew tap Homebrew/bundle
-  brew bundle --file $DOTFILES/macos/brew/fonts
-fi
-
-if ask "install util"; then
-  running "installing util"
-  brew tap Homebrew/bundle
-  brew bundle --file $DOTFILES/macos/brew/util
-fi
+for file in $DOTFILES/macos/brew/*
+do
+  filename=$(basename "$file")
+  filename="${filename%.*}"
+  if ask "install $filename"; then
+    brew tap Homebrew/bundle
+    brew bundle --file=$file
+  fi
+done
