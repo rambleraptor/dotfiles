e47e9b99c60371a1e41d99399a2d4e446f8d9040
Alex Stephen <alexstephen@google.com>
2017-08-17 23:20:10 -0700

    moved over some bash prompt code and cleaned up zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,86 +2,28 @@ autoload -U colors && colors
 # cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-# Git Prompt
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
+git_prompt(){
+  # Check if the current directory is in a Git repository.
+  if [[ "$(git rev-parse --is-inside-work-tree &>/dev/null; echo "${?}")" == '0' ]]; then
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+    # Get the short symbolic ref.
+    # If HEAD isn’t a symbolic ref, get the short SHA for the latest commit
+    # Otherwise, just give up.
+    branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+      git rev-parse --short HEAD 2> /dev/null || \
+      echo '(unknown)')";
 
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "(%{$fg[green]%}$(git_prompt_info)%{$reset_color%}$(need_push))"
+    if [[ $(git status --porcelain) == "" ]]; then
+      branchColor="%{$fg[green]%}"
     else
-      echo "(%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}$(need_push))"
+      branchColor="%{$fg_bold[red]%}"
     fi
-  fi
-}
 
-git_prompt_info () {
-# ref=$($git symbolic-ref HEAD 2>/dev/null) || return
- ref=$($git symbolic-ref HEAD 2>/dev/null)
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- if [[ -z  ${ref// }  ]]; then
-   branch=$(git branch --contains ${ref} | tail -1)
-   if [[ ! $branch == *"HEAD"* ]]; then
-     echo "${branch}" | tr -d '[:space:]'
-   else
-     ref=$($git rev-parse HEAD)
-     echo "${ref}"
-   fi
- else
-   echo "${ref#refs/heads/}"
- fi
-}
+    echo "(${branchColor}${branchName}%{$reset_color%})"
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo ""
   else
-    if [[ "$(uname -s)" == "Darwin" ]] then
-      echo "|🔥 "
-    else 
-      echo "|%{$fg_bold[magenta]%}unpushed%{$reset_color%}"
-    fi
-  fi
-}
-
-# Ruby Prompt
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
+    return;
+  fi;
 }
 
 # Current Directory
@@ -89,42 +31,29 @@ directory_name() {
   echo "%{$fg[cyan]%}%~%{$reset_color%}"
 }
 
-# Current Username
-server_name(){
-  local user=`whoami`
-  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CONNECTION" ]]
-  then
-    echo "%F{blue}[$USER@%m]%f "
-  elif [[ "$(uname -s)" == "Darwin" ]]
-  then
-    echo "🚀  ";
+cloud(){
+  if grep -q "^flags.* hypervisor" /proc/cpuinfo 2> /dev/null; then
+    echo "☁️  "
   else
-    echo "";
+    echo ""
+  fi
+}
+
+server_name(){
+  if [[ -n $DEFAULT_USER ]]; then
+    echo "🚀  "
+  else
+    echo "$(cloud)[\u@\H]"
   fi
 }
 
 prompt_arrow(){
   echo "%{$fg_bold[magenta]%}›%{$reset_color%}"
 }
-extra_spaces(){
-  PROMPT_SPACE=$(server_name)
-  if [[ $PROMPT_SPACE == *"🚀 "* ]] then
-    echo "  "
-  else
-    echo " "
-  fi
-}
 
 # Actual prompt
-export PROMPT=$'\n$(server_name)$(directory_name)$(git_dirty) $(prompt_arrow) '
-set_prompt () {
-  export RPROMPT="%{$fg[magenta]%}%{$reset_color%}"
-}
+export PROMPT=$'\n$(server_name)$(directory_name)$(git_prompt) $(prompt_arrow) '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
 }
-
-
-
