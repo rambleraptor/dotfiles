b2313030c27850047f0f5a83742f64e42495d038
Alex Stephen <alexstephen@google.com>
2017-08-20 00:28:00 -0700

    cleaned up vim package manager and gitignore mess

--- vim/install.sh
+++ vim/install.sh
@@ -2,9 +2,6 @@
 # include my library helpers for colorized echo and require_brew, etc
 source $HOME/.dotfiles/bootstrap/lib/helpers.sh
 
-running "adding vundle because submodules are awful"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 running "Symlinking all of the vim files"
 for source in `find ~/.dotfiles -path ~/.dotfiles/vim -prune -o -name '*.vim' -print`
 do
@@ -14,4 +11,4 @@ do
 done
 
 running "installing vundle plugins"
-vim +PluginInstall +qall!
+vim +PlugInstall +qall!
