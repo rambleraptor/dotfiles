ae539e1a99c64545bd44300c5d773fd6b25147bf
Alex Stephen <alexstephen@alexstephen-macbookpro2.roam.corp.google.com>
2020-02-19 14:11:50 -0800

    key repeat

--- macos/install/defaults.sh
+++ macos/install/defaults.sh
@@ -38,6 +38,9 @@ sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo Hos
 # Trackpad, keyboard
 #######################################
 
+running "Disable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
 running "Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
