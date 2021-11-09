95da9958c9f688cb235aec4f82804b6e8f80dd04
Alex Stephen <astephen2@gmail.com>
2021-11-09 15:12:36 -0800

    removing old stuff

--- googlecloud/gcloud.bash
+++
@@ -1,9 +0,0 @@
-# Gcloud SSH
-function gcloud-tunnel(){
-  gcloud compute ssh $1 --ssh-flag="-L $2:localhost:$2"
-}
-
-alias gssh="gcloud compute ssh $1"
-
-alias gcompute="gcloud compute"
-alias gcp="gcloud compute copy-files $1 $2~"
