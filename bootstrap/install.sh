1cf6f9bb83e4235842dad3ed890bface568ea87b
Alex Stephen <astephen2@gmail.com>
2021-11-11 18:50:29 -0800

    remove all the vim nonsense I don't need

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -20,6 +20,7 @@ $DOTFILES/bootstrap/lib/symlink.sh
 # Install vim-plug
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
   if ask "Install vim-plug?"; then
+    mkdir -p $HOME/.vim/autoload
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
