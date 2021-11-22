56a26e5e78e58497a1758bd9b8d834d9c00b4ca5
Alex Stephen <astephen2@gmail.com>
2021-11-21 22:10:57 -0800

    add pyenv to path file

--- path.bash
+++ path.bash
@@ -4,12 +4,19 @@ if [ -d "$HOME/.rbenv" ]; then
   eval "$(rbenv init -)"
 fi
 
+# Adding pyenv to PATH
+if [ -d "$HOME/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+fi
+
 # Adding Go to PATH
 if [ -d "$HOME/go" ]; then
   export GOPATH="$HOME/go"
   export PATH="$PATH:$HOME/go/bin"
 fi
 
+# Local path file
 if [ -f "$DOTFILES/local/path.bash" ]; then
   source $DOTFILES/local/path.bash
 fi
