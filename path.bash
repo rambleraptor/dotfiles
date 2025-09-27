4a992ece51f7811405ad6fd01148bface6ddd3fc
Alex Stephen <astephen2@gmail.com>
2025-09-27 16:11:33 -0700

    clean up zsh

--- path.bash
+++ path.bash
@@ -1,21 +1,3 @@
-# Adding Rbenv to PATH
-if [ -d "$HOME/.rbenv" ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-fi
-
-# Adding pyenv to PATH
-if [ -d "$HOME/.pyenv" ]; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-fi
-
-# Adding Go to PATH
-if [ -d "$HOME/go" ]; then
-  export GOPATH="$HOME/go"
-  export PATH="$PATH:$HOME/go/bin"
-fi
-
 # Local path file
 if [ -f "$DOTFILES/local/path.bash" ]; then
   source $DOTFILES/local/path.bash
@@ -26,3 +8,5 @@ export PATH=$PATH:$DOTFILES/bin
 
 # Local binary folders
 export PATH=$PATH:$DOTFILES/local/bin
+
+eval "$(mise activate zsh)"
